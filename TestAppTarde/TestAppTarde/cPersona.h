47c9442f04e1e19e6316ebe5b9d518688200265f
DMLON <damiandeza@gmail.com>
2020-06-05 14:38:01 -0300

    Persona

---
+++ TestAppTarde/TestAppTarde/cPersona.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+using namespace std;
+class cPersona
+{
+	string nombre;
+	string apellido;
+public:
+	cPersona(string nom, string ap) :nombre(nom), apellido(ap) {};
+};
+
